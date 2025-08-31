f56afc3c6749b13e45792fc6987faa63ced3595b
wenyu <wenyuwang@deloittecn.com.cn>
2025-08-31 17:13:16 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-susuTest_Tests/Pods-susuTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_susuTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_susuTest_Tests
+@end
---
+++ Example/Pods/Target Support Files/susuTest/susuTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_susuTest : NSObject
+@end
+@implementation PodsDummy_susuTest
+@end
